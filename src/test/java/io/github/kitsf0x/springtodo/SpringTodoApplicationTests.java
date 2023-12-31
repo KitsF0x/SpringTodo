5c8940388b58cbfd74a427be959b1af8cb184db0
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-12-31 22:28:08 +0100

    Init

---
+++ src/test/java/io/github/kitsf0x/springtodo/SpringTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kitsf0x.springtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
